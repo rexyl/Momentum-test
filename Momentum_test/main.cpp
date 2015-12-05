ffcfc3bf7b940dc05c1a8da62fe19c46169a1ae2
rexy <ly2352@columbia.edu>
2015-12-05 01:19:13 -0500

    Initial Commit

---
+++ Momentum_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Momentum_test
+//
+//  Created by Lu Yang on 12/5/15.
+//  Copyright © 2015 Lu Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
